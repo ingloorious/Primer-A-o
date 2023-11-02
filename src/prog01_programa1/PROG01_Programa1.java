182b8149b2ab3fe46e475c922283683176d562f3
Joseda <Joseda@DESKTOP-4NFH7RO.home>
2023-11-02 16:35:38 +0100

    Programa

---
+++ src/prog01_programa1/PROG01_Programa1.java
@@ -0,0 +1,19 @@
+
+package prog01_programa1;
+
+public class PROG01_Programa1 {
+
+ 
+    public static void main(String[] args) {
+        
+        System.out.println("Módulo Profesional - PROGRAMACIÓN. UNIDAD DE TRABAJO 01\n"
+                + "Introduccion a la programación\n"
+                + "Jose Daniel Osorio Ante\n"
+                + "Localidad : Albacete y provincia : Albacete\n"
+                + "26/10/2023\n"
+                + "Programa1");
+
+     
+    }
+    
+}
